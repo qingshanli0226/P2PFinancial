cff75756557bac94c5584b98ca04343f19cbf062
Your Name <you@example.com>
2019-11-21 13:35:08 +0800

    修改一点加载页的bug

--- app/src/main/java/com/example/administrator/p2pdemotext/Util/PageUtil.java
+++ app/src/main/java/com/example/administrator/p2pdemotext/Util/PageUtil.java
@@ -48,7 +48,7 @@ public class PageUtil {
 
     }
     public void hideload(){
-        if (!isloading){
+        if (isloading){
             getReview().removeView(inflate);
             isloading=!isloading;
 
